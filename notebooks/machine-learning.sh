15b7f35e42637935e02f36cefa80e96935f01d96
Xipeng Wang <xipengw@umich.edu>
2021-05-07 22:25:34 -0400

    Blog[ML]: Loss functions.

---
+++ notebooks/machine-learning.sh
@@ -0,0 +1,4 @@
+./converter.py --nbpath machine-learning-summary.ipynb --date 2021-05-03 --description "Machine Learning Notes" --tag ML
+./converter.py --nbpath machine-learning-pytorch.ipynb --date 2021-05-04 --description "Machine Learning Notes - pytorch basics" --tag ML
+./converter.py --nbpath machine-learning-loss.ipynb --date 2021-05-07 --description "Machine Learning Notes - loss functions, hinge loss(svm loss), softmax loss" --tag ML
+./converter.py --nbpath machine-learning-backpropagation.ipynb --date 2021-05-07 --description "Machine Learning Notes - gradient descent, backpropagation" --tag ML
